7063e46b7169443f71606ed9634926a6ce990215
jbigorra <juanchoibf@gmail.com>
2020-04-13 13:16:32 +0200

    Loops class.
    Co-authored-by: lulusadowsky <sadowska.luiza@gmail.com>

--- src/csv-to-json-parser.spec.js
+++ src/csv-to-json-parser.spec.js
@@ -1,12 +1,15 @@
 const TSV = ' id \t name \t email \n12323123\tPablo Poroto\tp.p@gmail.com\n432423423\tMaria Craig\tmaria.c@gmail.com';
 
-function tsvToArray () {
-
+function tsvToArray (tsvString) {
+  return tsvString.split('\n');
 }
 
+// we need to pass a parameter to the tsvToArray function from TSV
+// we transform the data in the parameter from string to array by splitting by EOL character
+// return the array
 describe('csvToArray', () => {
   describe('Given each line of a tsv file ends with an endOfLine character', () => { // endOfLine = \n
-    it('Should convert csv to array', () => {
+    it('Should convert csv to array by splitting by EOL character', () => {
       // Given
       const tsvString = TSV;
 
@@ -20,9 +23,29 @@ describe('csvToArray', () => {
   });
 });
 
-function parseHeaders () {
+function parseHeaders (array) {
+  const dirtyHeaders = array[0].split('\t');
 
+  const cleanHeaders = removeTrailingSpacesFrom(dirtyHeaders);
+
+  return cleanHeaders;
 }
+
+function removeTrailingSpacesFrom (dirtyHeaders) {
+  const cleanHeaders = [];
+
+  for (let i = 0; i < dirtyHeaders.length; i++) {
+    const element = dirtyHeaders[i];
+    const clean = element.trim();
+    cleanHeaders.push(clean);
+  }
+
+  return cleanHeaders;
+};
+
+// within the given array we point at the first element [0] (headers string)
+// we take only this data [0] - headers string
+// turn it into array by splitting by \t
 
 describe('parseHeaders', () => {
   describe('Given a tsv string turned into an array', () => {
---
+++ src/loops.js
@@ -0,0 +1,81 @@
+const arrayWithNumbers = [14, 16, 22, 15, 5];
+
+// Logs the full array
+console.log('Full array:');
+console.log(arrayWithNumbers);
+console.log('\n');
+
+// Log manually each item of the array
+console.log('Each item manually:');
+console.log(arrayWithNumbers[0]);
+console.log(arrayWithNumbers[1]);
+console.log(arrayWithNumbers[2]);
+console.log(arrayWithNumbers[3]);
+console.log(arrayWithNumbers[4]);
+console.log('\n');
+
+// Log each item of the array dynamically with a for loop
+console.log('Each item with for loop: array length is: ' + arrayWithNumbers.length + '\n');
+for (let i = 0; i < arrayWithNumbers.length; i++) {
+  // Accessing each element of arrayWithNumbers.
+  // Explanation: On each iteration variable "i" is increasing its value by 1.
+  // which means, we can use the "i" to access each element of the array dynamically.
+  console.log(`i = ${i} and arrayWithNumbers[${i}]: ${arrayWithNumbers[i]}`);
+  console.log(arrayWithNumbers[i]);
+}
+
+console.log('\n');
+
+// when I use const instead of var/let; I am declaring a variable with value that can't be change
+// meaning I can't reasign a new object/string etc.
+// when I declare const within a scope of for loop/function etc it exists only inside of the for loop;
+// whenever I put a condition with for loop, it has to be > or < , eventually <= never === !!!!!!!!!!!
+
+// Log each item of the array dynamically with a while loop
+// when using a while loop the index declaration has to be outside of while loop
+// and the increasing of the index has to be done inside of the while loop
+console.log("Each item with 'while' loop: array length is: " + arrayWithNumbers.length + '\n');
+
+let index = 0;
+while (index < arrayWithNumbers.length) {
+  // Accessing each element of arrayWithNumbers.
+  // Explanation: On each iteration variable "i" is increasing its value by 1.
+  // which means, we can use the "i" to access each element of the array dynamically.
+  console.log(`i = ${index} and arrayWithNumbers[${index}]: ${arrayWithNumbers[index]}`);
+  console.log(arrayWithNumbers[index]);
+  index++;
+  // index = index + 1;
+  // index += 1;
+}
+
+// do-while loops will run the first iteration no matter what and then evualuate the condition
+// to go to the next iteration.
+
+console.log("\nEach item with 'do - while' loop: array length is: " + arrayWithNumbers.length + '\n');
+
+let index2 = 0;
+do {
+  // Accessing each element of arrayWithNumbers.
+  // Explanation: On each iteration variable "i" is increasing its value by 1.
+  // which means, we can use the "i" to access each element of the array dynamically.
+  console.log(`i = ${index2} and arrayWithNumbers[${index2}]: ${arrayWithNumbers[index2]}`);
+  console.log(arrayWithNumbers[index2]);
+  index2++;
+} while (index2 < arrayWithNumbers.length);
+
+// More complex exercise would be something like:
+
+// function User (name, lastname) {
+//   this.name = name;
+//   this.lastname = lastname;
+// }
+
+// const user1 = new User('juan', ' bigorra');
+// const user2 = new User('pablo', 'perez');
+
+// const array = [user1, user2];
+// const array2 = [];
+// array2.push(user1, user2);
+
+// console.log(array);
+// console.log(array2);
